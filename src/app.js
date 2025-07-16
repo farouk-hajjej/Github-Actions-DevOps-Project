a1a2bf01b68b9f46f911b6126a499c4fc354552c
farouk-hajjej <74997699+farouk-hajjej@users.noreply.github.com>
2025-07-16 16:27:44 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ function greet(name) {
 }
 
 module.exports = greet; // Ensure function is exportable
-// If run directly, print output (useful for debugging)
+// Iif run directly, print output (useful for debugging)
 
 if (require.main === module) {
   console.log(greet("World"));
